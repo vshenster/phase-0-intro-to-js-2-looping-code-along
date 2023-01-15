44a6e7b269059fe684fedb4184f3320c74913904
Vicky Shen <vicshe01@gmail.com>
2023-01-15 18:30:43 -0500

    commit tree 4eba1450d3a32e65018f91f39b6ab49af9183b39

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message)
+    }
+    return messages;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali", 'Wes'], "christmas"))
+
+
+function countDown() {
+    //console.log('In function')
+    let startingNumber = 10;
+    while (startingNumber >= 0) {
+        console.log(startingNumber--)
+        
+    }
+
+    //countDown();
+}
+
+
+/*function countDown() {
+    let x = 10;
+    while (x >= 0) {
+        console.log(x--);
+    }
+}*/
+
+countDown()
